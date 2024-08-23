c50944196d2b0a8ec54fb53d41239b07a49b5b11
shaoun-01 <heyshaoun@gmail.com>
2024-08-24 00:29:42 +0600

    first commit

---
+++ views/layout/AppLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+import SimpleChatbot from '../components/SimpleChatBot';
+
+
+const AppLayout = ({ children }) => {
+    return (
+        <>
+            <Toaster position='top-center' />
+            
+            {children}
+          <SimpleChatbot></SimpleChatbot>
+        </>
+    );
+};
+
+export default AppLayout;
